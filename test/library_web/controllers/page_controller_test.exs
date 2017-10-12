3250d8e9c214fa40194259ec922632a61e2f0b9a
finnhodgkin <finnhodgkin@gmail.com>
2017-10-12 17:57:36 +0100

    removes phoenix branding. Adds dwyl favicon #2

--- test/library_web/controllers/page_controller_test.exs
+++ test/library_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule LibraryWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "dwyl | Library"
   end
 end
