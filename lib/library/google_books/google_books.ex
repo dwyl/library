2e446f1ff71314938422c33a30af7cb93824cb41
finnhodgkin <finnhodgkin@gmail.com>
2017-10-17 18:30:42 +0100

    adds google_books module to search and validate the API #13

--- lib/library/google_books/google_books.ex
+++ lib/library/google_books/google_books.ex
@@ -1,7 +1,12 @@
 defmodule Library.GoogleBooks do
   @book_api_url "https://www.googleapis.com/books/v1/volumes?key=#{System.get_env "GOOGLE_BOOKS_KEY"}&q="
   @desired_info ["title", "authors", "publishedDate", "printType", "categories", ["imageLinks", "thumbnail"], ["imageLinks", "smallThumbnail"], "language", "previewLink", "industryIdentifiers"]
-
+  @keys_to_rename [{"title", :title}, {"authors", :author_list},
+                   {"publishedDate", :date_published}, {"printType", :type},
+                   {"categories", :category}, {"thumbnail", :thumbnail},
+                   {"smallThumbnail", :thumbnail_small},
+                   {"language", :language}, {"previewLink", :preview_link},
+                   {"ISBN_13", :isbn_10}, {"ISBN_10", :isbn_13}]
   @httpoison Application.get_env(:library, :httpoison) || HTTPoison
 
   @doc """
@@ -15,46 +20,60 @@ defmodule Library.GoogleBooks do
       |> handle_google_api_response
   end
 
-def handle_google_api_response(result) do
-  case result do
-    {:ok, result} ->
-      result
-      |> Map.get(:body)
-      |> Poison.decode
-      |> handle_api_decode
-    {:err, error} ->
-      error # TODO: do something proper with this error
-  end
-end
-
-def handle_api_decode(result) do
-  case result do
-    {:ok, map} ->
-      Map.get(map, "items")
-      |> Enum.map(fn %{"volumeInfo" => map} ->
-        map
-        |> get_values_from_map(@desired_info)
-        |> get_isbns
-      end)
-    {:err, error} ->
-      error # TODO: do something proper with this error
-  end
-end
-
-defp get_isbns(%{"industryIdentifiers" => identifiers} = map) do
-  Enum.reduce(identifiers, %{}, fn %{"identifier" => isbn, "type" => type}, acc ->
-    Map.put(acc, type, isbn)
-  end)
-  |> Map.merge(map)
-  |> Map.delete("industryIdentifiers")
-end
-
-defp get_isbns(map), do: map
-
   @doc """
-    Takes a search query and a category, and creates a URL for google books based on this
+
   """
-  defp create_url(search_query, search_category) do
+  defp handle_google_api_response(result) do
+    case result do
+      {:ok, result} ->
+        result
+        |> Map.get(:body)
+        |> Poison.decode
+        |> handle_api_decode
+      {:error, _error} = error ->
+        error
+    end
+  end
+
+  def handle_api_decode(result) do
+    case result do
+      {:ok, map} ->
+        Map.get(map, "items")
+        |> Enum.reduce([], fn (%{"volumeInfo" => map}, acc) ->
+          map
+          |> get_values_from_map(@desired_info)
+          |> get_isbns
+          |> rename_keys(@keys_to_rename)
+          |> Map.put(:owned, false)
+          |> validate_required
+          |> case do
+            {:ok, map} ->
+              acc ++ [map]
+            {:error, error} ->
+              acc
+          end
+        end)
+      {:error, error} ->
+        error # TODO: do something proper with this error
+    end
+  end
+
+  defp validate_required(%{title: _title,
+                          author_list: _authors,
+                          owned: _owned} = map), do: {:ok, map}
+  defp validate_required(map), do: {:error, "Missing valid property"}
+
+  defp get_isbns(%{"industryIdentifiers" => identifiers} = map) do
+    Enum.reduce(identifiers, %{}, fn %{"identifier" => isbn, "type" => type}, acc ->
+      Map.put(acc, type, isbn)
+    end)
+    |> Map.merge(map)
+    |> Map.delete("industryIdentifiers")
+  end
+
+  defp get_isbns(map), do: map
+
+  def create_url(search_query, search_category) do
     query = URI.encode(search_query)
     @book_api_url <> case search_category do
       "title" ->
@@ -83,12 +102,16 @@ defp get_isbns(map), do: map
         map_to_return
       value ->
         Map.put(map_to_return, final_key, value)
-      end
+    end
   end
 
   def get_if_value([head | tail], map, map_to_return) do
-    value = Map.get(map, head)
-    get_if_value(tail, value, map_to_return)
+    case Map.get(map, head) do
+      nil ->
+        map_to_return
+      value ->
+        get_if_value(tail, value, map_to_return)
+    end
   end
 
   def get_if_value(string, map, map_to_return) do
@@ -100,8 +123,16 @@ defp get_isbns(map), do: map
     end
   end
 
-  @doc """
-    takes a map
-  """
+  def rename_keys(map, list) do
+    list
+    |> Enum.reduce(%{}, fn {old, new}, acc ->
+      case Map.get(map, old) do
+        nil ->
+          acc
+        value ->
+          Map.put(acc, new, value)
+      end
+    end)
+  end
 
 end
